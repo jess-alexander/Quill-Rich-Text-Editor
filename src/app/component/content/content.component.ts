e0da65838046529e563ce4d3bf2a67ab5d7a0936
jess-alexander <alexanderjessmarie@gmail.com>
2019-08-17 11:38:34 -0500

    1 file modified

--- src/app/component/content/content.component.ts
+++ src/app/component/content/content.component.ts
@@ -84,9 +84,8 @@ export class ContentComponent implements OnInit {
     if(valid == false) return false;
     valid = this.validContent('rgb','Text color detected, please clear formatting');
     if(valid == false) return false;
-    valid = this.validContent('nbsp','\'&nbsp\' detected, search text blurb for location');
-  
-    if(valid == false) return false;
+    valid = this.validContent('nbsp','\'&nbsp\' detected, search text blurb for location')
+    if(valid == false) this.removeNBSP();
     else return true;
   }
 
@@ -96,5 +95,9 @@ export class ContentComponent implements OnInit {
       alert(errorMessage);
     }
     return !result;
+  }
+
+  removeNBSP(){
+    this.pageConfig['content'].replace('&nbsp','');
   }
 }
